57b737c8a2474118fa2f130147ff2160638662d8
galyahikbariyeh <ghaliahjehad66@gmail.com>
2024-11-02 22:10:31 +0300

    done

---
+++ js/index1.js
@@ -0,0 +1,32 @@
+alert("good")
+var age=20
+age=20
+console.log('age is',age)
+
+let age1=69
+age1=20
+console.log('age1 is',age1)
+
+const age2=12
+
+console.log("age2 is",age2)
+test="this is test"
+console.log('test :',test)
+ var a=9
+ var c=2
+ var z=a+c
+ console.log(z)
+
+ var isActive=true
+ console.log("is :",isActive)
+
+ var names=["ali","aya","6",4]
+ console.log(names[8])
+ var sample= null
+ console.log("samole is:",sample)
+//=== value and data type == just value
+ if(age2 == 12){
+    console.log("true condition")
+ }else{
+    console.log("false condition")
+ }
