c23cd4cccee7c2d4a03ef7693a56fee2ecb44696
galyahikbariyeh <ghaliahjehad66@gmail.com>
2024-12-04 18:24:33 +0300

    js 1

---
+++ js/new.js
@@ -0,0 +1,29 @@
+console.log('hello')
+var age1 =5;
+age1 =7
+console.log('global:',age1)
+
+let age2= 43;
+age2=54
+console.log('local:',age2)
+
+const age3=8;
+console.log('const:',age3)
+ test=9
+ console.log('test:',test)
+
+ var isActive  =true
+ console.log('active:',isActive)
+
+ var names=["ali","tala","sara"]
+ console.log('names:',names)
+ var s
+ console.log('s:',s)
+//== comp value === comp value and data type
+ if(age1 == 7){
+    console.log('success')
+ }else{
+    console.log('failed')
+ }
+
+
